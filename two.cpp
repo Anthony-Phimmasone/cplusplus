1be51155952251e1ab40cffbb751a92eb59f3b67
Anthony-Phimmasone <31628907+Anthony-Phimmasone@users.noreply.github.com>
2019-04-19 15:19:41 -0400

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int n;
+
+    cin >> a >> b >> c;
+    n = a + b +c;
+    cout << n << endl;
+    return 0;
+}
